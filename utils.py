c4700a9011f46fa57cb3c0116924fb0c7e4f5709
Griffen <griffen@gr1ffen.dk>
2024-09-22 08:18:53 +0200

    Refactor: Logging system rework

--- utils.py
+++ utils.py
@@ -19,27 +19,27 @@ mod_from_backup = True
 def get_version_and_download():
     #Get most current version
     if not os.path.isdir(join(installLocation, MOD_FOLDER)):
-        log_output('Mod not downloaded yet', 'warning')
+        logger.warning('Mod not downloaded yet')
         get_zip_from_github()
     else:
         if get_online_version() != get_current_version():
-            log_output('Local mod outdated, getting new version', 'warning')
-            log_output(f'Online version: {get_online_version()}, Local version: {get_current_version()}', 'warning')
+            logger.warning('Local mod outdated, getting new version')
+            logger.warning(f'Online version: {get_online_version()}, Local version: {get_current_version()}')
             get_zip_from_github()
         else:
-            log_output('Local mod is most updated version', 'info')
+            logger.info('Local mod is most updated version')
 
 def get_zip_from_github():
     """
     Downloads zip from latest release, extracts to temporary download folder, renames it, moves it, deletes temps
     """
-    log_output('Downloading from GitHub, do not close...', 'info')
+    logger.info('Downloading from GitHub, do not close...')
     download_url = json.loads(requests.get('https://api.github.com/repos/Noob-Development/Sandbox-Mod-Files/releases/latest', allow_redirects=True).content)['zipball_url']
     urllib.request.urlretrieve(download_url, os.path.join(installLocation, 'master.zip'))
-    log_output('Extracting Zip', 'info')
+    logger.info('Extracting Zip')
     with zipfile.ZipFile(join(installLocation, 'master.zip'), 'r') as zip_ref:
         zip_ref.extractall(os.path.join(installLocation, 'download'))
-    log_output('Moving folders', 'info')
+    logger.info('Moving folders')
     download_name = list(os.listdir(installLocation + '\\' + 'download'))[0]
     if os.path.isdir(join(installLocation, MOD_FOLDER)):
         rmtree(join(installLocation, MOD_FOLDER))
